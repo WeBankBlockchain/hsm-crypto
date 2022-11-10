57051b449d401b294c97f06cb455ef8f4872db42
lucasli1024 <410567249@qq.com>
2022-11-10 16:18:53 +0800

    wrap functions for hash_init/hash_update/hash_final

--- hsm/sdf/SDFCryptoProvider.cpp
+++ hsm/sdf/SDFCryptoProvider.cpp
@@ -1,6 +1,5 @@
 #include "SDFCryptoProvider.h"
 #include "../Common.h"
-#include "gmt0018.h"
 #include <stdio.h>
 #include <condition_variable>
 #include <cstdlib>
@@ -228,6 +227,30 @@ unsigned int SDFCryptoProvider::KeyGen(AlgorithmType algorithm, Key* key)
     default:
         return SDR_ALGNOTSUPPORT;
     }
+}
+
+unsigned int SDFCryptoProvider::HashInit(SGD_HANDLE sessionHandle, ECCrefPublicKey *pucPublicKey)
+{
+    SGD_RV code;
+    if (pucPublicKey == nullptr)
+    {
+        code = SDF_HashInit(sessionHandle, SGD_SM3, NULL, NULL, 0);
+    }
+    else
+    {
+        code = SDF_HashInit(sessionHandle, SGD_SM3, pucPublicKey,(unsigned char*)SM2_USER_ID.c_str(),16);
+    }
+    return code;
+}
+
+unsigned int SDFCryptoProvider::HashUpdate(SGD_HANDLE sessionHandle, unsigned char* message, unsigned int messageLen)
+{
+    return SDF_HashUpdate(sessionHandle, message, messageLen);
+}
+
+unsigned int SDFCryptoProvider::HashFinal(SGD_HANDLE sessionHandle, unsigned char* digest, unsigned int* digestLen)
+{
+    return SDF_HashFinal(sessionHandle, digest, digestLen);
 }
 
 unsigned int SDFCryptoProvider::Hash(Key* key, AlgorithmType algorithm,
