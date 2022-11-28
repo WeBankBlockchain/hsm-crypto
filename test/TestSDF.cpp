73f6558452ab9fad76a0a75fd1347ed50551d623
lucasli <410567249@qq.com>
2022-11-28 17:45:48 +0800

    remove using namespace std (#21)
    * add namespace in common.h && add generate random API
    
    * wrap functions for hash_init/hash_update/hash_final
    
    * remove function named HashInit/HashUpdate/HashFinal
    
    * remove using namespace std

--- test/TestSDF.cpp
+++ test/TestSDF.cpp
@@ -5,7 +5,6 @@
 #include <string>
 #include <thread>
 
-using namespace std;
 using namespace hsm;
 using namespace hsm::sdf;
 
@@ -18,7 +17,7 @@ void callCard(int inum)
         unsigned int code = provider.KeyGen(SM2, &key);
         if (code != SDR_OK)
         {
-            cout << provider.GetErrorMessage(code) << endl;
+            std::cout << provider.GetErrorMessage(code) << std::endl;
         }
     }
 }
@@ -40,9 +39,9 @@ int hextostr(const unsigned char* hex, unsigned int hlen, unsigned char* str)
 
 int main(int, const char* argv[]){
     // Crypto provider 测试
-    cout << "**************Begin Test, bash test-sdf-crypto [sessionPoolSize] "
+    std::cout << "**************Begin Test, bash test-sdf-crypto [sessionPoolSize] "
             "[loopRound]************************"
-         << endl;
+         << std::endl;
     size_t sessionPoolRound = atoi(argv[1]);
     size_t loopRound = atoi(argv[2]);
     try
@@ -51,13 +50,13 @@ int main(int, const char* argv[]){
     }
     catch (const std::exception& e)
     {
-        cout << "error occured, info: " << e.what() << endl;
+        std::cout << "error occured, info: " << e.what() << std::endl;
         exit(1);
     }
     SDFCryptoProvider& provider = SDFCryptoProvider::GetInstance(sessionPoolRound);
 
     // Make hash
-    cout << "**************Make SM3 Hash************************"<<endl;
+    std::cout << "**************Make SM3 Hash************************"<< std::endl;
     std::vector<byte> bHashVector = {0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,
                                 0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,
                                 0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,
@@ -67,75 +66,75 @@ int main(int, const char* argv[]){
                                     0x6f,0xdb,0x70,0xe5,0x38,0x7e,0x57,0x65,0x29,0x3d,0xcb,0xa3,0x9c,0x0c,0x57,0x32};
     SDFCryptoResult result = Hash(nullptr, SM3, sdfToHex(bHashVector));
     if (result.sdfErrorMessage != nullptr){
-        cout << "Get error : " << result.sdfErrorMessage <<endl;
+        std::cout << "Get error : " << result.sdfErrorMessage << std::endl;
     }else{
-        cout << "Get Hash : " << result.hash << endl;
+        std::cout << "Get Hash : " << result.hash << std::endl;
 
-        cout << "Standard : " << sdfToHex(bHashStdResultVector) << endl;
+        std::cout << "Standard : " << sdfToHex(bHashStdResultVector) << std::endl;
     }
 
-    cout << "****KeyGen****" << endl;
+    std::cout << "****KeyGen****" << std::endl;
     result = KeyGen(SM2);
     if (result.sdfErrorMessage != nullptr){
-        cout << "Get error : " << result.sdfErrorMessage <<endl;
+        std::cout << "Get error : " << result.sdfErrorMessage << std::endl;
     }else{
-        cout << "Get public key : " << result.publicKey << endl;
-        cout << "Get private key : " << result.privateKey << endl;
+        std::cout << "Get public key : " << result.publicKey << std::endl;
+        std::cout << "Get private key : " << result.privateKey << std::endl;
     }
     
-    cout << "**************Make SM2_SM3 Hash************************"<<endl;
+    std::cout << "**************Make SM2_SM3 Hash************************"<< std::endl;
     SDFCryptoResult sm2_sm3_result = Hash(result.publicKey, SM3, sdfToHex(bHashVector));
     if (sm2_sm3_result.sdfErrorMessage != nullptr)
     {
-        cout << "Get error : " << sm2_sm3_result.sdfErrorMessage << endl;
+        std::cout << "Get error : " << sm2_sm3_result.sdfErrorMessage << std::endl;
     }
     else
     {
-        cout << "SM2_SM3 : " << sm2_sm3_result.hash << endl;
+        std::cout << "SM2_SM3 : " << sm2_sm3_result.hash << std::endl;
     }
 
-    cout << "****Sign****" << endl;
+    std::cout << "****Sign****" << std::endl;
     SDFCryptoResult signResult;
     signResult = Sign(result.privateKey, SM2, sdfToHex(bHashStdResultVector));
     if (signResult.sdfErrorMessage != nullptr){
-        cout << "Get error : " << signResult.sdfErrorMessage <<endl;
+        std::cout << "Get error : " << signResult.sdfErrorMessage << std::endl;
     }else{
-        cout << "Get signature: " << signResult.signature << endl;
+        std::cout << "Get signature: " << signResult.signature << std::endl;
     }
 
-    cout << "****Verify****" << endl;
+    std::cout << "****Verify****" << std::endl;
     SDFCryptoResult verifyResult;
     verifyResult = Verify(
         result.publicKey, SM2, hsm::sdf::sdfToHex(bHashStdResultVector), signResult.signature);
     if (verifyResult.sdfErrorMessage != nullptr){
-        cout << "Get error : " << verifyResult.sdfErrorMessage <<endl;
+        std::cout << "Get error : " << verifyResult.sdfErrorMessage << std::endl;
     }else{
-        cout << "Get verify result: " << verifyResult.result << endl;
+        std::cout << "Get verify result: " << verifyResult.result << std::endl;
     }
 
-    cout << "****SignInternalKey****" << endl;
+    std::cout << "****SignInternalKey****" << std::endl;
     signResult = SignWithInternalKey(1, "12345678", SM2, sdfToHex(bHashStdResultVector));
     if (signResult.sdfErrorMessage != nullptr){
-        cout << "Get error : " << signResult.sdfErrorMessage <<endl;
+        std::cout << "Get error : " << signResult.sdfErrorMessage << std::endl;
     }else{
-        cout << "Get signature: " << signResult.signature << endl;
+        std::cout << "Get signature: " << signResult.signature << std::endl;
     }
 
-    cout << "****VerifyInternalKey****" << endl;
+    std::cout << "****VerifyInternalKey****" << std::endl;
     verifyResult = VerifyWithInternalKey(
         1, SM2, (const char*)sdfToHex(bHashStdResultVector), signResult.signature);
     if (verifyResult.sdfErrorMessage != nullptr){
-        cout << "Get error : " << verifyResult.sdfErrorMessage <<endl;
+        std::cout << "Get error : " << verifyResult.sdfErrorMessage << std::endl;
     }else{
-        cout << "Get verify result: " << verifyResult.result << endl;
+        std::cout << "Get verify result: " << verifyResult.result << std::endl;
     }
 
-    cout << "*****ExportInternalPublicKey****" << endl;
+    std::cout << "*****ExportInternalPublicKey****" << std::endl;
     SDFCryptoResult exportResult = ExportInternalPublicKey(1, SM2);
-    cout << "Export public key: " << exportResult.publicKey << endl;
+    std::cout << "Export public key: " << exportResult.publicKey << std::endl;
 
 
-    cout << "*****SM4 Encrypt****" << endl;
+    std::cout << "*****SM4 Encrypt****" << std::endl;
     const std::vector<byte> pkv = {0x01, 0x23, 0x45, 0x67,
             0x89, 0xab, 0xcd, 0xef, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10};
     std::shared_ptr<const std::vector<byte>> pbKeyValue =
@@ -146,8 +145,8 @@ int main(int, const char* argv[]){
 	std::vector<byte> pbPlainText = {0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef,0xfe,0xdc,0xba,0x98,0x76,0x54,0x32,0x10,0x29,0xbe,0xe1,0xd6,0x52,0x49,0xf1,0xe9,0xb3,0xdb,0x87,0x3e,0x24,0x0d,0x06,0x47};
 	std::vector<byte> pbCipherText = {0x3f,0x1e,0x73,0xc3,0xdf,0xd5,0xa1,0x32,0x88,0x2f,0xe6,0x9d,0x99,0x6c,0xde,0x93,0x54,0x99,0x09,0x5d,0xde,0x68,0x99,0x5b,0x4d,0x70,0xf2,0x30,0x9f,0x2e,0xf1,0xb7};
 
-    cout << "Plain Text: " << sdfToHex(pbPlainText) << endl;
-    cout << "IV        : " << sdfToHex(pbIV) << endl;
+    std::cout << "Plain Text: " << sdfToHex(pbPlainText) << std::endl;
+    std::cout << "IV        : " << sdfToHex(pbIV) << std::endl;
     Key key = Key();
     key.setSymmetricKey(pbKeyValue);
     unsigned int cypherLen;
@@ -155,54 +154,54 @@ int main(int, const char* argv[]){
     unsigned int encryptCode  = provider.Encrypt(key, SM4_CBC ,pbIV.data(), pbPlainText.data(),
         32, cypher.data(), &cypherLen);
     if (encryptCode != SDR_OK) {
-      cout << "Failed!!" <<endl;
-      cout << provider.GetErrorMessage(encryptCode) <<endl;
+      std::cout << "Failed!!" << std::endl;
+      std::cout << provider.GetErrorMessage(encryptCode) << std::endl;
     } else {
-        cout << "Encrypt Result  : " << sdfToHex(cypher) << endl;
-        cout << "Standard Result : " << sdfToHex(pbCipherText) << endl;
+        std::cout << "Encrypt Result  : " << sdfToHex(cypher) << std::endl;
+        std::cout << "Standard Result : " << sdfToHex(pbCipherText) << std::endl;
     }
 
 
-    cout << "*****SM4 Decrypt****" << endl;
-    cout << "Cipher Text: " << sdfToHex(pbPlainText) << endl;
-    cout << "IV         : " << sdfToHex(originIV) << endl;
+    std::cout << "*****SM4 Decrypt****" << std::endl;
+    std::cout << "Cipher Text: " << sdfToHex(pbPlainText) << std::endl;
+    std::cout << "IV         : " << sdfToHex(originIV) << std::endl;
     std::vector<byte> plain(32);
     unsigned int plainlen;
     unsigned int decryptoCode = provider.Decrypt(
         key, SM4_CBC, originIV.data(), pbCipherText.data(), 32, plain.data(), &plainlen);
     if (decryptoCode != SDR_OK) {
-      cout << "Failed!!" <<endl;
-      cout << provider.GetErrorMessage(decryptoCode) <<endl;
+      std::cout << "Failed!!" << std::endl;
+      std::cout << provider.GetErrorMessage(decryptoCode) << std::endl;
     } else {
-        cout << "Decrypt Result  : " << sdfToHex(plain) << endl;
-        cout << "Standard Result : " << sdfToHex(pbPlainText) << endl;
+        std::cout << "Decrypt Result  : " << sdfToHex(plain) << std::endl;
+        std::cout << "Standard Result : " << sdfToHex(pbPlainText) << std::endl;
     }
 
-    cout << "*****generate Random Num****" << endl;
+    std::cout << "*****generate Random Num****" << std::endl;
     unsigned int randomLength = 64;
     unsigned char ucRandom[1024+1] = {0};
     unsigned char Rand1[1024+1] = {0};
     if (provider.generateRandom(randomLength, ucRandom))
     {
         hextostr(ucRandom,64,Rand1);
-        cout << "generate random number success :" << Rand1 << endl;
+        std::cout << "generate random number success :" << Rand1 << std::endl;
     }
     else
     {
-        cout << "generate random number failed :" << endl;
+        std::cout << "generate random number failed :" << std::endl;
     }
     
 
-    cout << "******Prallel test******" << endl;
-    vector<thread> callCardThread;
+    std::cout << "******Prallel test******" << std::endl;
+    std::vector<std::thread> callCardThread;
     for (int i = 0; i < sessionPoolRound; i++)
     {
-        callCardThread.push_back(thread(callCard, loopRound));
+        callCardThread.push_back(std::thread(callCard, loopRound));
     }
     for (auto iter = callCardThread.begin(); iter!= callCardThread.end(); iter++)
 	{
 		iter->join();  
 	}
-	cout << "prallel test finished " << endl;
+	std::cout << "prallel test finished " << std::endl;
     return 0;
 }
